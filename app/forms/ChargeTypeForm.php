e3ff9fc643c73210daac862a6db065539bbe8688
Denis Zharkov <bintree@eblans.org>
2012-05-28 16:56:16 +0700

    Форма типов начислений

--- app/forms/ChargeTypeForm.php
+++ app/forms/ChargeTypeForm.php
@@ -1,8 +1,9 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: sufix
- * Date: 28.05.12
- * Time: 16:51
- * To change this template use File | Settings | File Templates.
- */
+
+class ChargeTypeForm extends mModelForm {
+	protected function configure()
+	{
+		$this->setElement('name', new mInputTextFormElement());
+		$this->setAttributeValidator('name', new mValidatorString());
+	}
+}
