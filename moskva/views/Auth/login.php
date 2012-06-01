09b2903a09a958f0943b14660346e851d4f56f3e
Denis Zharkov <bintree@eblans.org>
2012-06-01 18:32:24 +0700

    Hotfix

--- moskva/views/Auth/login.php
+++ moskva/views/Auth/login.php
@@ -3,6 +3,6 @@
 	<div class="error">Неправильные логин/пароль</div>
 	<?php endif; ?>
 	Логин: <input type="text" name="m_login" value="<?php echo isset($_POST['m_login']) ? $_POST['m_login'] : ''; ?>"><br/>
-	Пароль: <input type="password" name="m_password" value="<?php echo isset($_POST['m_login']) ? $_POST['m_login'] : ''; ?>"><br/>
+	Пароль: <input type="password" name="m_password" value="<?php echo isset($_POST['m_password']) ? $_POST['m_password'] : ''; ?>"><br/>
 	<input type="submit" value="Войти">
 </form>
